f01d44ae712e50b80e86f4288942c768f7d23ae7
Mario Echeverria <soyox2037@gmail.com>
2022-05-31 16:18:31 -0400

    commit

--- src/routes/OrderDetails.jsx
+++ src/routes/OrderDetails.jsx
@@ -6,12 +6,17 @@ import ButtonOutline from "../components/buttons/ButtonOutline";
 import SpinnerLoader from "../components/SpinnerLoader";
 import H1Compontent from "../components/titles/H1Compontent";
 import Swal from "sweetalert2";
-import { async } from "@firebase/util";
 
 function OrderDetails() {
   const { id } = useParams();
-  const { getSingleOrder, loading, data, deleteData, paymenteManager } =
-    useDB();
+  const {
+    getSingleOrder,
+    loading,
+    data,
+    deleteData,
+    paymenteManager,
+    resultManager,
+  } = useDB();
 
   useEffect(() => {
     getSingleOrder(id);
@@ -41,13 +46,19 @@ function OrderDetails() {
 
   const handlePayment = async () => {
     const { value: formValues } = await Swal.fire({
-      title: "Ingrese el pago",
+      title: "Datos del pago",
       html:
         "<h3> Estatus del Pago</h3>" +
-        '<select id="paymentStatusform" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> <option disabled selected value="1"> Seleccione Uno </option> <option value="aprobado"> Aprobado </option><option value="negado"> Negado </option> </select>' +
+        '<select required id="paymentStatusform" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> <option disabled selected value="1"> Seleccione Uno </option> <option value="aprobado"> Aprobado </option><option value="negado"> Negado </option> </select>' +
         "<br>" +
         "<h3> Tipo de Moneda </h3>" +
-        '<select id="paymentCurrency" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> <option disabled selected value="1"> Seleccione Uno </option><option value="dolares"> Dolares </option><option value="bolivares">Bolivares </option> </select>',
+        '<select id="paymentCurrency" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> <option disabled selected value="1"> Seleccione Uno </option><option value="dolares"> Dolares </option><option value="bolivares">Bolivares </option> </select>' +
+        "<br>" +
+        "<h3> Ingrese monto del pago </h3>" +
+        '<input type="number" id="amaunt" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">' +
+        "<br>" +
+        "<h3> Referencia </h3>" +
+        '<textarea id="ref" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>',
       focusConfirm: false,
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
@@ -56,6 +67,8 @@ function OrderDetails() {
           {
             status: document.getElementById("paymentStatusform").value,
             currency: document.getElementById("paymentCurrency").value,
+            ref: document.getElementById("ref").value,
+            amaunt: document.getElementById("amaunt").value,
           },
         ];
       },
@@ -63,28 +76,102 @@ function OrderDetails() {
 
     if (formValues) {
       if (formValues[0].status == 1) {
-        Swal.fire("Error!", "Debe seleccionar un status.", "info")
-        .then((result) => {
-          if(result.isConfirmed) {
-            handlePayment()
+        Swal.fire("Error!", "Debe seleccionar un status.", "info").then(
+          (result) => {
+            if (result.isConfirmed) {
+              handlePayment();
+            }
           }
-        });
-        return
+        );
+        return;
       }
       if (formValues[0].currency == 1) {
-        Swal.fire("Error!", "Debe seleccionar una moneda", "info")
-        .then((result) => {
-          if(result.isConfirmed) {
-            handlePayment()
+        Swal.fire("Error!", "Debe seleccionar una moneda", "info").then(
+          (result) => {
+            if (result.isConfirmed) {
+              handlePayment();
+            }
+          }
+        );
+        return;
+      }
+      if (formValues[0].amaunt == "") {
+        Swal.fire("Error!", "Debe ingresar un monto", "info").then((result) => {
+          if (result.isConfirmed) {
+            handlePayment();
           }
         });
-        return
+        return;
       }
 
-      Swal.fire("Exito!", "Pago cargado", "success")
+      Swal.fire( {title: "Exito!",
+      text: "Resultado cargado",
+      icon: "success",
+      confirmButtonColor: "#3085d6",
+      timer: 2000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      
+      });
       console.log(JSON.stringify(formValues));
-    } 
+      paymenteManager(
+        formValues[0].status,
+        formValues[0].currency,
+        formValues[0].ref,
+        formValues[0].amaunt,
+        id
+      );
+    }
   };
+
+  const handleResult = async () => {
+    const { value: formValues } = await Swal.fire({
+      title: "Evaluar Muestra",
+      html:
+        "<h3> Resultado </h3>" +
+        '<select id="testresult" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"> <option disabled selected value="1"> Seleccione Uno </option><option value="Positivo"> Positivo </option><option value="Negativo">Negativo </option> </select>',
+      focusConfirm: false,
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      preConfirm: () => {
+        return [
+          {
+            result: document.getElementById("testresult").value,
+          },
+        ];
+      },
+    });
+
+    if (formValues) {
+      if (formValues[0].result == 1) {
+        Swal.fire({
+          title: "Error!",
+          text: "Debe seleccionar un status.",
+          icon: "info",
+          confirmButtonColor: "#3085d6",
+        }).then((result) => {
+          if (result.isConfirmed) {
+            handleResult();
+          }
+        });
+        return;
+      }
+
+      Swal.fire({
+        title: "Exito!",
+        text: "Resultado cargado",
+        icon: "success",
+        confirmButtonColor: "#3085d6",
+        timer: 2000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+      });
+
+      console.log(JSON.stringify(formValues));
+      resultManager(formValues[0].result, id);
+    }
+  };
+
   return (
     <>
       <div>
@@ -119,9 +206,8 @@ function OrderDetails() {
                 <p className="text-xl pb-4">{data.testtype}</p>
                 <p className="text-xs">Estado del pago:</p>
                 <p className="align-middle text-xl">
-                  {data.paymentStatus}{" "}
+                  {data.paymentStatus}
                   <span>
-                    {" "}
                     <ButtonOutline
                       onClick={() => {
                         handlePayment();
@@ -129,18 +215,18 @@ function OrderDetails() {
                       text="Agregar Pago"
                     />
                   </span>
-                  <span>
-                    
-                    <ButtonOutline text="Modificar Pago" />
-                  </span>
                 </p>
                 <p className="text-xs">Resultado: </p>
                 <p className="text-xl pb-4">
-                  {data.testResult}{" "}
+                  {data.testResult}
                   <span>
-                    {" "}
-                    <ButtonOutline text="Modificar Resultado" />{" "}
-                  </span>{" "}
+                    <ButtonOutline
+                      text="Cargar Resultado"
+                      onClick={() => {
+                        handleResult();
+                      }}
+                    />
+                  </span>
                 </p>
               </div>
               <div className="item w-4/12 h-32 flex-grow">
