26e438aae62ed20d1a77dc09a71a65e06a209f56
Mario Echeverria <soyox2037@gmail.com>
2022-06-01 17:45:24 -0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   content: ['./src/**/*.html', './node_modules/flowbite/**/*.js'],
+  theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+  },
   plugins: [
     require('flowbite/plugin')
   ],
