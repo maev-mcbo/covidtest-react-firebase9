440de5b192fd3a071801a6539f7172b9f8ca85ae
Mario Echeverria <soyox2037@gmail.com>
2022-06-01 17:48:48 -0400

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   theme: {
     screens: {
       'xs': '475px',
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
       
     },
   },
