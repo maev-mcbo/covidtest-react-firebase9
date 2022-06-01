b8927f27915da28c60006cc49ec96d45205f340a
Mario Echeverria <soyox2037@gmail.com>
2022-06-01 18:14:51 -0400

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,98 +21,120 @@ const Navbar = () => {
 
   return (
     <>
-<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-  <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="https://flowbite.com" className="flex items-center">
-      <img
-        src="/docs/images/logo.svg"
-        className="mr-3 h-6 sm:h-9"
-        alt="Flowbite Logo"
-      />
-      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-        Flowbite
-      </span>
-    </a>
-    <button
-      data-collapse-toggle="mobile-menu"
-      type="button"
-      className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      aria-controls="mobile-menu"
-      aria-expanded="false"
-    >
-      <span className="sr-only">Open main menu</span>
-      <svg
-        className="w-6 h-6"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-          clipRule="evenodd"
-        />
-      </svg>
-      <svg
-        className="hidden w-6 h-6"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fillRule="evenodd"
-          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-          clipRule="evenodd"
-        />
-      </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
-      <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-        <li>
+<nav className="bg-white shadow-lg">
+  <div className="max-w-6xl mx-auto px-4">
+    <div className="flex justify-between">
+      <div className="flex space-x-7">
+        <div>
+          {/* Website Logo */}
+          <a href="#" className="flex items-center py-4 px-2">
+            <img src="logo.png" alt="Logo" className="h-8 w-8 mr-2" />
+            <span className="font-semibold text-gray-500 text-lg">
+              Navigation
+            </span>
+          </a>
+        </div>
+        {/* Primary Navbar items */}
+        <div className="hidden md:flex items-center space-x-1">
           <a
-            href="#"
-            className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-            aria-current="page"
+            href=""
+            className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
           >
             Home
           </a>
-        </li>
-        <li>
           <a
-            href="#"
-            className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            href=""
+            className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
           >
             Services
           </a>
-        </li>
-        <li>
           <a
-            href="#"
-            className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+            href=""
+            className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
           >
-            Pricing
+            About
           </a>
-        </li>
-        <li>
           <a
-            href="#"
-            className="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            href=""
+            className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
           >
-            Contact
+            Contact Us
           </a>
-        </li>
-      </ul>
+        </div>
+      </div>
+      {/* Secondary Navbar items */}
+      <div className="hidden md:flex items-center space-x-3 ">
+        <a
+          href=""
+          className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300"
+        >
+          Log In
+        </a>
+        <a
+          href=""
+          className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300"
+        >
+          Sign Up
+        </a>
+      </div>
+      {/* Mobile menu button */}
+      <div className="md:hidden flex items-center">
+        <button className="outline-none mobile-menu-button">
+          <svg
+            className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+            x-show="!showMenu"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
+  {/* mobile menu */}
+  <div className="hidden mobile-menu">
+    <ul className="">
+      <li className="active">
+        <a
+          href="index.html"
+          className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+        >
+          Home
+        </a>
+      </li>
+      <li>
+        <a
+          href="#services"
+          className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+        >
+          Services
+        </a>
+      </li>
+      <li>
+        <a
+          href="#about"
+          className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+        >
+          About
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+        >
+          Contact Us
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
+
 
     </>
   );
